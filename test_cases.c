4e594152c7b0785658af3dad8d5fa602afb7bc6e
APAC\bki8cob <balasubramanian.krishnamoorthy@in.bosch.com>
2023-01-23 17:12:51 +0530

    Added basic test case

--- test_cases.c
+++ test_cases.c
@@ -3,11 +3,13 @@
 #include <assert.h>
 #include "rangeProvider.h"
 
-void main(void)
+int main(void)
 {
     int data[]={4,5};
     char output[100];
 
     getRangeData(data, 2, output);
     assert(strcmp(output,"Range, Readings\n4-5, 2\n") == 0);
+
+    return 0;
 }
