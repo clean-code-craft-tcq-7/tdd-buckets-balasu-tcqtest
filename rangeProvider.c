4e79182f4fd604a51b1e234046eb1e320c936dc6
APAC\bki8cob <balasubramanian.krishnamoorthy@in.bosch.com>
2023-01-31 14:21:48 +0530

    Added code to check emoty array as input

--- rangeProvider.c
+++ rangeProvider.c
@@ -9,8 +9,12 @@ void getRangeData(int* data, int dataCnt, char* buff)
     int cntInRange;
 
     memset(printData, 0, 100);
-    sortInAscending(data,dataCnt);
-    cntInRange = findCountInRange(data, dataCnt, data[0], data[dataCnt - 1]);
-    sprintf(printData,"%s\n%d-%d, %d\n",HEADER_CHAR,data[0],data[dataCnt - 1], cntInRange);
+    sprintf(printData,"%s\n",HEADER_CHAR);
+    if((data != NULL) && (dataCnt > 0))
+    {
+        sortInAscending(data,dataCnt);
+        cntInRange = findCountInRange(data, dataCnt, data[0], data[dataCnt - 1]);
+        sprintf(printData,"%s\n%d-%d, %d\n",HEADER_CHAR,data[0],data[dataCnt - 1], cntInRange);
+    }
     strncpy(buff,printData,strlen(printData));
 }
