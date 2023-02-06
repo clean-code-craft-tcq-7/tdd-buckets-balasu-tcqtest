eec3506937b3fa20a05395f13b1dd638b2ec9347
APAC\bki8cob <balasubramanian.krishnamoorthy@in.bosch.com>
2023-02-06 18:47:40 +0530

    Code updated for round of feature

--- a2dconverter.c
+++ a2dconverter.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include "a2dconverter.h"
 
 
 int convertA2D(int* data, int len, float* output)
 {
     int count = 0, outIdx = 0;
+    float tempVal;
+
     while(count < len)
     {
         if(checkValidSample(data[count]))
         {
-            output[outIdx] = (float)((float)MAX_AMP_VALUE * ((float)data[count] / (float)ADC_MAX_VALUE));
+            tempVal =(float)((float)MAX_AMP_VALUE * ((float)data[count] / (float)ADC_MAX_VALUE));
+            output[outIdx] = round(tempVal);
             outIdx++;
         }
         count++;
--- test_cases_a2dconverter.c
+++ test_cases_a2dconverter.c
@@ -7,14 +7,14 @@ void test_cases_a2dconverter(void)
 {
     // Test case to check the input to adc converter and output with single element
     int data[] = {4000, 3000};
-    float result[] = {9.770395, 7.327797};
+    float result[] = {10, 7};
     float output[2];
     convertA2D(data, 2, output);
     assert(memcmp(output,result,2)==0);
 
     // Test to check the converter and the converter avoids invalid sensor values
     int data1[] = {4000, 3000, 5000};
-    float result1[] = {9.770395, 7.327797};
+    float result1[] = {10, 7};
     float output1[2];
     int outCnt = 0;
     outCnt = convertA2D(data1, 2, output1);
@@ -30,10 +30,10 @@ void test_cases_a2dconverter(void)
 
     // Test the sample conversion with the round of values
     int data2[] = {4000, 3000};
-    float result2[] = {10.0, 7.0};
+    float result2[] = {10, 7};
     float output2[2];
     int outCnt2 = 0;
-    outCnt = convertA2D(data2, 2, output2);
+    outCnt2 = convertA2D(data2, 2, output2);
     assert(memcmp(output2,result2,2)==0);
     assert(outCnt2 == 2);
 
