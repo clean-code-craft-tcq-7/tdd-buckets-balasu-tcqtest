233d093b6510db13f21f041fb4af007879fbdc48
APAC\bki8cob <balasubramanian.krishnamoorthy@in.bosch.com>
2023-02-08 10:03:59 +0530

    Added more test cases

--- test_case_rangeProviderWithADC.c
+++ test_case_rangeProviderWithADC.c
@@ -12,4 +12,26 @@ void test_case_rangeProviderWithADC(void)
     memset(output, 0, 100);
     getRangeData(data5, 2, output);
     assert(strcmp(output, "Range, Readings\n4-5, 2\n") == 0);
+
+    //Test case with empty array as input
+    int data2[2];
+    memset(output, 0, 100);
+    getRangeData(data2, 0, output);
+    assert(strcmp(output, "Range, Readings\n") == 0);
+
+    //Test case with empty array as input but valid length value
+    memset(output, 0, 100);
+    getRangeData(NULL, 2, output);
+    assert(strcmp(output, "Range, Readings\n") == 0);
+
+    //Test case with empty array as input and invalid length value
+    memset(output, 0, 100);
+    getRangeData(NULL, 0, output);
+    assert(strcmp(output, "Range, Readings\n") == 0);
+
+    //Test case with multiple ranges
+    int data3[] = { 1200, 1228, 2047, 1637};
+    memset(output, 0, 100);
+    getRangeData(data3, 4, output);
+    assert(strcmp(output, "Range, Readings\n3-5, 4\n") == 0);
 }
